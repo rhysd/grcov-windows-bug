eebba8663b6df76690dc5a28bf330161496b3c9c
rhysd <lin90162@yahoo.co.jp>
2022-04-09 20:53:24 +0900

    add anyhow

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-pub fn hello() {
+use anyhow::Result;
+
+pub fn hello() -> Result<()> {
     println!("hello");
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        super::hello();
+        super::hello().unwrap();
     }
 }
